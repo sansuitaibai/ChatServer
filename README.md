8f4da79c466894ea7444a75fda54fc2f02ba0514
ghy <2657302908@qq.com>
2023-07-25 09:49:47 +0800

    add autocompile.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ChatServer
+基于muduo库实现网络层， nginx tcp负载均衡， redis的发布订阅的跨服务器通信的 集群聊天服务器
+
+手动编译：
+`cd build`
+`rm -rf ./*`
+`cmake ..`
+`make`
+
+自动编译:
--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,9 @@
 
-
+#!/usr/bin/bash
 set -x
 
-rm -rf 'pwd'/build/*
-cd 'pwd'/build && 
+rm -rf `pwd`/build
+mkdir `pwd`/build
+cd `pwd`/build && 
     cmake .. &&
     make 
